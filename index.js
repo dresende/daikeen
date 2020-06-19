81d5716bf79454c3e1ff79626f256ea156806e2d
Diogo Resende <dresende@thinkdigital.pt>
2020-06-19 19:47:24 +0100

    exports Modes and FanRates

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const Connection = require("./lib/Connection");
+const Data       = require("./lib/Data");
 
 exports.createConnection = (address) => {
 	return new Connection(address, {
 		interval : 5000,
 	});
 };
+
+exports.Modes    = Data.modes;
+exports.FanRates = Data.fan_rates;
